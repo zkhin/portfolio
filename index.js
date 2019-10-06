6d24b92bbb96123d2b5f1f35d1034c5669ed69ac
zkhin <arandomname@gmail.com>
2019-10-06 17:20:09 -0400

    updates

---
+++ index.js
@@ -0,0 +1,16 @@
+function handleToggleNav() {
+    $('.topnav').on('click', '.icon', (event) => {
+        event.preventDefault();
+        var x = document.getElementById("topnav");
+        if (x.className === "topnav") {
+        x.className += " responsive";
+        } else {
+        x.className = "topnav";
+        }
+    })
+    
+} 
+
+
+  
+$(handleToggleNav);

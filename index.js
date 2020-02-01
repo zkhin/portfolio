f2623bab60b114b90329b8c3a10c7e4a998044fd
zkhin <arandomname@gmail.com>
2020-01-31 21:37:44 -0500

    update

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 function handleToggleNav() {
-    $('.topnav').on('click', '.icon', (event) => {
-        event.preventDefault();
-        var x = document.getElementById("topnav");
-        if (x.className === "topnav") {
-        x.className += " responsive";
-        } else {
-        x.className = "topnav";
-        }
-    })
-    
-} 
+  $(".topnav").on("click", ".icon", event => {
+    event.preventDefault();
+    var x = document.getElementById("topnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  });
+}
 
-
-  
 $(handleToggleNav);
